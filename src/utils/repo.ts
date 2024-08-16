8e3e78556eab6d15e9eb3edd7b4ba9c5fac57e8d
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-16 18:28:37 +0700

    fix: fix url justd repo

--- src/utils/repo.ts
+++ src/utils/repo.ts
@@ -1,5 +1,5 @@
 export const getRepoUrlForComponent = (componentName: string) => {
-  const repoUrl = `https://raw.githubusercontent.com/irsyadadl/justd/master/components/ui/${componentName}.tsx`
+  const repoUrl = `https://raw.githubusercontent.com/justdlabs/justd/master/components/ui/${componentName}.tsx`
   if (!repoUrl) {
     throw new Error('REPO_URL environment variable is not set')
   }
