f6e14471e50dc4484a7dcb8735f59e5a03e7e77a
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-09-02 21:33:35 +0700

    fix: fix range picker

--- src/resources/components.ts
+++ src/resources/components.ts
@@ -150,7 +150,7 @@ const components: any[] = [
   { name: 'calendar', children: [{ name: 'button' }] },
   { name: 'range-calendar', children: [{ name: 'calendar' }] },
   { name: 'date-field' },
-  { name: 'date-range-picker', children: [{ name: 'date-picker' }, { name: 'range-calendar' }] },
+  { name: 'date-range-picker', children: [{ name: 'date-picker' }] },
   {
     name: 'date-picker',
     children: [
